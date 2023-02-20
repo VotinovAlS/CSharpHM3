2bee26cc295d881531773b27c3fb8f143d9690ea
Nick <nick00712@yandex.ru>
2023-02-21 01:00:55 +0500

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N >0)
+{
+for(int i = 1; i <= N; i++)
+{
+    Console.Write(Math.Pow(i, 3) + "\t");
+}
+}
+else
+{
+  for(int i = -1; i >= N; i--)
+{
+    Console.Write(Math.Pow(i, 3) + "\t");
+}
+}
